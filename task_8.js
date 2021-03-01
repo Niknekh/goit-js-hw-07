0c1e2fc4cba847952040288e8cae94d904886648
Mykyta Nekh <nikita.nekh@gmail.com>
2021-03-01 18:03:35 +0200

    done 2

--- task_8.js
+++ task_8.js
@@ -7,6 +7,7 @@ const refs = {
 let result = 0;
 refs.input.addEventListener('input', event => result = event.target.valueAsNumber)
 refs.render.addEventListener('click', event => createBoxes(result))
+refs.destroy.addEventListener('click', event => destroyBoxes())
 
 function createBoxes(amount) {
     for (let i = 0; i < amount; i += 1){
@@ -15,10 +16,13 @@ function createBoxes(amount) {
         newDiv.style.width = `${30 + i* 10}px`;
         newDiv.style.height = `${30 + i* 10}px`;
 
-        boxes.appendChild(newDiv)
+        refs.boxes.appendChild(newDiv)
     }
 }
 
 function randomColor() {
     return "#" + Math.random().toString(16).slice(2,8);
+}
+function destroyBoxes() {
+    return refs.boxes.innerHTML = '';
 }
